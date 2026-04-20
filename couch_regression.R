f961d919f21970f666b946fde1d77ecf87389d30
Jason-Staats <JEStaats86@gmail.com>
2026-04-19 23:04:13 -0400

    Add files via upload

---
+++ couch_regression.R
@@ -0,0 +1,17 @@
+# Load libraries
+library(dplyr)
+library(tidyr)
+
+# Read data
+df <- read.csv("C:/Portfolio Projects/Exercise Dataset/activity_healthcare_agg.csv")
+
+# Model 1
+model1 <- lm(avg_spending ~ avg_muscle, data = df)
+cat("Model 1: Muscle-strengthening activities vs Healthcare Spending\n")
+summary(model1)
+
+
+# Model 2
+model2 <- lm(avg_spending ~ avg_no_leisure, data = df)
+cat("Model 2: No leisure-time activity vs Healthcare Spending\n")
+summary(model2)
